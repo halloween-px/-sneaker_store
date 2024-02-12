0fdd9ad9e69cd82aedf01300354f1d606a4d3983
halloween-px <84710696+halloween-px@users.noreply.github.com>
2024-02-12 21:52:27 +0300

    bro sneaker

---
+++ sneaker-store/src/components/product/CartList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+        title="Кросовки крутые 1"
+        :price="5000"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <Card
+        title="Кросовки крутые 2"
+        :price="5000"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <Card
+        title="Кросовки крутые 3"
+        :price="5000"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <Card
+        title="Кросовки крутые 4"
+        :price="5000"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <Card
+        title="Кросовки крутые 5"
+        :price="5000"
+        img="/sneakers/sneakers-1.jpg"
+    />
+  </div>
+</template>
