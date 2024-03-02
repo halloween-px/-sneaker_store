773e7b40f77701cef939b5f0155cb972f1b2907c
halloween-px <84710696+halloween-px@users.noreply.github.com>
2024-03-02 17:31:44 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  publicPath: "/-sneaker_store/",
+  base:'/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
